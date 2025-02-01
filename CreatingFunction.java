c04d49c361b35d46783f6d18f28dea4b12d22e40
nirmal523 <kadanirmalkumar@gmail.com>
2025-02-01 18:45:27 +0530

    Add files via upload

---
+++ CreatingFunction.java
@@ -0,0 +1,15 @@
+class CreatingFunction{
+    public static void main(String[] args) {
+      System.out.println("this is an function ")  ;
+      int result =shiva(4,5);
+System.out.println("the sum of two numbers is "+result);
+    }
+
+    static int shiva(int a,int b){
+      int sum=a+b;
+      return sum;
+    }
+ 
+
+}
+ 
